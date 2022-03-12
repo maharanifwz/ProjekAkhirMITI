d9d1139ad3783157d88786eb5b85db99d85be8d7
maharanifwz <maharanifwz@student.ub.ac.id>
2022-03-12 17:59:15 +0700

    seventh commit

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,149 +1,149 @@
-// import React from 'react'
-// import { Form, Input, Button } from 'antd';
-// import styled from 'styled-components';
-// import { ButtonContainer } from '../components/home/Hero';
-// import { HashLink as Link } from 'react-router-hash-link';
-// import { useAuth } from '../config/Auth';
-// import { useNavigate } from 'react-router-dom';
-// import { useState } from 'react';
-// import Axios from 'axios';
+import React from 'react'
+import { Form, Input, Button } from 'antd';
+import styled from 'styled-components';
+import { ButtonContainer } from '../components/home/Hero';
+import { HashLink as Link } from 'react-router-hash-link';
+import { useAuth } from '../config/Auth';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Axios from 'axios';
 
-// const ErrorMessage = styled.p`
-// 	color: red;
-// 	font-size: 1.5rem;
-// 	margin-bottom: 1rem;
-// 	padding: 0 2rem;
-// 	text-align: center;
-// `;
+const ErrorMessage = styled.p`
+	color: red;
+	font-size: 1.5rem;
+	margin-bottom: 1rem;
+	padding: 0 2rem;
+	text-align: center;
+`;
 
-// const FormSignIn = styled.div`
-//     .ant-input-affix-wrapper" {
-//         border-color: #829C5F;
-//         border-radius: 10px 0px 0px 10px;
-//     }
-//     .ant-col ant-form-item-control {
-//         border-color: #829C5F;
-//         border-radius: 10px 0px 0px 10px;
-//         background: orange;
+const FormSignIn = styled.div`
+    .ant-input-affix-wrapper" {
+        border-color: #829C5F;
+        border-radius: 10px 0px 0px 10px;
+    }
+    .ant-col ant-form-item-control {
+        border-color: #829C5F;
+        border-radius: 10px 0px 0px 10px;
+        background: orange;
     
-//     }
-//     .ant-form-item-control-input {
-//         border-color: #829C5F;
-//         border-radius: 10px 0px 0px 10px; 
-//         background:#829C5F;
-//     }
-// `;
+    }
+    .ant-form-item-control-input {
+        border-color: #829C5F;
+        border-radius: 10px 0px 0px 10px; 
+        background:#829C5F;
+    }
+`;
 
 
 
-// export const Signin = () => {
-//     const {setAndGetTokens} = useAuth();
-//     const [forms, setForms] = useState({ Username: '', Password: '' });
-//     const [isError, setIsError] = useState({ status: false, message: '' });
-//     const navigate = useNavigate();
+export const Signin = () => {
+    const {setAndGetTokens} = useAuth();
+    const [forms, setForms] = useState({ Username: '', Password: '' });
+    const [isError, setIsError] = useState({ status: false, message: '' });
+    const navigate = useNavigate();
 
-//     const handleLogin = async (e) => {
-//         e.preventDefault();
+    const handleLogin = async (e) => {
+        e.preventDefault();
 
-//     try {
-//         //tweetAPI bisa dilihat di file api.js
-//         const loginResponse = await Axios.post('https://e483-118-99-76-148.ngrok.io/user/login', {
-//             ...forms,
-//         });
+    try {
+        //tweetAPI bisa dilihat di file api.js
+        const loginResponse = await Axios.post('https://e483-118-99-76-148.ngrok.io/user/login', {
+            ...forms,
+        });
 
-//         console.log(loginResponse);
-//         //jika loginnya sukses
-//         if (loginResponse.data.success) {
-//             //ambil tokennya
-//             /*data yg pertama itu data dari loginResponsenya, 
-//             data yang kedua data hasi keluaran dari responnya 
-//             coba cek link yg ada di api.js*/
-//             const token = loginResponse.data.data.token;
-//             //ambil id user yang udh sempet dia register
-//             const currUser = await Axios.get( {
-//                 headers: {
-//                     Authorization: `Bearer ${token}`,
-//                 },
-//             });
+        console.log(loginResponse);
+        //jika loginnya sukses
+        if (loginResponse.data.success) {
+            //ambil tokennya
+            /*data yg pertama itu data dari loginResponsenya, 
+            data yang kedua data hasi keluaran dari responnya 
+            coba cek link yg ada di api.js*/
+            const token = loginResponse.data.data.token;
+            //ambil id user yang udh sempet dia register
+            const currUser = await Axios.get( {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
 
-//             //ambil id usernya
-//             const id = currUser.data.data.id;
+            //ambil id usernya
+            const id = currUser.data.data.id;
 
-//             //parameternya diisi sesuai dgn parameter yg ada di app.js
-//             //set ke local storage
-//             setAndGetTokens(token, id);
+            //parameternya diisi sesuai dgn parameter yg ada di app.js
+            //set ke local storage
+            setAndGetTokens(token, id);
 
-//             //redirect ke homepage
-//             navigate('/', { replace: true });
-//         }
-//     }
-//         catch(error) {
-//             setIsError((isError) => ({
-//                 status: true,
-//                 message: "Error while try to log in",
-//             }));
-//         }
-//     };
+            //redirect ke homepage
+            navigate('/', { replace: true });
+        }
+    }
+        catch(error) {
+            setIsError((isError) => ({
+                status: true,
+                message: "Error while try to log in",
+            }));
+        }
+    };
 
-//   return (
-//     <div className='block signin height'>
-//        <div className='container-fluid'> 
-//             <FormSignIn>   
-//                 <Form onSubmit={handleLogin}
-//                     name="normal_login"
-//                     className="login-form"
-//                     initialValues={{ remember: true }}
-//                     style={{width: 325}}
-//                     >
-//                     <Form.Item><h2>Masuk</h2></Form.Item>
-//                     <Form.Item
-//                         name="username"
-//                         rules={[{ required: true, message: 'Mohon inputkan username Anda!' }]}
-//                     >Username
-//                         <Input 
-//                         placeholder="ex: maharanifwz@gmail.com" 
-//                         onChange={(e) => {
-//                             setForms(() => ({
-//                                 ...forms,
-//                                 Username: e.target.value
-//                             }))
-//                         }}/>
-//                     </Form.Item>
-//                     <Form.Item
-//                         name="password"
-//                         rules={[{ required: true, message: 'Mohon inputkan password Anda!' }]}
-//                     >Password
-//                         <Input.Password
-//                         onChange={(e) => {
-//                             setForms(() => ({
-//                                 ...forms,
-//                                 Password: e.target.value
-//                             }))
-//                         }}/>
-//                     </Form.Item>
-//                     <Form.Item>
-//                         <a className="login-form-forgot" href="">
-//                         Forgot password?
-//                         </a>
-//                     </Form.Item>
+  return (
+    <div className='block signin height'>
+       <div className='container-fluid'> 
+            <FormSignIn>   
+                <Form onSubmit={handleLogin}
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{ remember: true }}
+                    style={{width: 325}}
+                    >
+                    <Form.Item><h2>Masuk</h2></Form.Item>
+                    <Form.Item
+                        name="username"
+                        rules={[{ required: true, message: 'Mohon inputkan username Anda!' }]}
+                    >Username
+                        <Input 
+                        placeholder="ex: maharanifwz@gmail.com" 
+                        onChange={(e) => {
+                            setForms(() => ({
+                                ...forms,
+                                Username: e.target.value
+                            }))
+                        }}/>
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: 'Mohon inputkan password Anda!' }]}
+                    >Password
+                        <Input.Password
+                        onChange={(e) => {
+                            setForms(() => ({
+                                ...forms,
+                                Password: e.target.value
+                            }))
+                        }}/>
+                    </Form.Item>
+                    <Form.Item>
+                        <a className="login-form-forgot" href="">
+                        Forgot password?
+                        </a>
+                    </Form.Item>
 
-//                     <Form.Item>
-//                         <ButtonContainer>
-//                             <Button style={{width:300}} type="primary" htmlType="submit" className="login-form-button">
-//                             Masuk
-//                             </Button>
-//                         </ButtonContainer>
-//                     </Form.Item>
+                    <Form.Item>
+                        <ButtonContainer>
+                            <Button style={{width:300}} type="primary" htmlType="submit" className="login-form-button">
+                            Masuk
+                            </Button>
+                        </ButtonContainer>
+                    </Form.Item>
 
-//                     <Form.Item>
-//                         <center>Belum memiliki akun? <a><Link to='/signup' className='identity'>Daftar disini.</Link></a></center>
-//                     </Form.Item>
-//                 </Form>
-//             </FormSignIn>
-//             {isError.status && (
-//                         <ErrorMessage>{isError.message}</ErrorMessage>
-//                     )}
-//         </div>
-//     </div>
-//   )
-// }
+                    <Form.Item>
+                        <center>Belum memiliki akun? <a><Link to='/signup' className='identity'>Daftar disini.</Link></a></center>
+                    </Form.Item>
+                </Form>
+            </FormSignIn>
+            {isError.status && (
+                        <ErrorMessage>{isError.message}</ErrorMessage>
+                    )}
+        </div>
+    </div>
+  )
+}
